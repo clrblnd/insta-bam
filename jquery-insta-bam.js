a36d63ef0fa4dfefdfd0c3ca8b13666c08a5bb99
Hans Spooren <hans@mrhenry.be>
2013-01-30 10:40:44 +0100

    compiled code

--- jquery-insta-bam.js
+++ jquery-insta-bam.js
@@ -2,7 +2,7 @@
  * insta-bam
  *
  * Created at: 2012-11-20 21:22:09 +0100
- * Updated at: 2012-11-26 18:09:54 +0100
+ * Updated at: 2013-01-30 10:38:44 +0100
  *
  * Author: @clrblnd (+ @ivow for the way of the code)
  * Version: 1.2.1
